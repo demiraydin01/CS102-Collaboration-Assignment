dcfc041345c36239d33b2010dd2c265e0a9cb4d2
erendirican <erendirican2007@gmail.com>
2026-01-28 15:32:06 +0300

    Implement computeaverage method in collabWork
    Added a method to compute the average and differences from it.

--- collabWork.java
+++ collabWork.java
@@ -1,5 +1,16 @@
 public class collabWork {
-
+    public static int[] computeaverage(int[] arr){
+        int sum = 0;
+        for(int i = 0; i<arr.length; i++){
+            sum += arr[i];
+        }
+        int average = sum / arr.length;
+        int[] diffarr = new int[arr.length];
+        for(int i = 0; i<arr.length; i++){
+            diffarr[i] = arr[i] - average;
+        }
+        return diffarr;
+    }
     public static void main(String[] args) {
 
         
