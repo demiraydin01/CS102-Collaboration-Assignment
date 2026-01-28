16cb0970fb15c282d7220d027570888b8e2c8509
demiraydin01 <demir.aydinx@gmail.com>
2026-01-28 15:34:41 +0300

    added createArray() function

--- collabWork.java
+++ collabWork.java
@@ -1,4 +1,17 @@
+import java.util.Random;
+
 public class collabWork {
+    
+    // returns a random array with the values [0,100] for a given length
+    public int[] createArray(int size){
+        int[] result = new int[size];
+        Random rand = new Random();
+        for(int i = 0; i < size; i++){
+            result[i] = rand.nextInt(0,101);
+        }
+        return result;
+    }
+
 
     public static void main(String[] args) {
 
