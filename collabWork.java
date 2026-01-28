ae25676f20e201fc5ef0004a6a71ff88a8d04e19
Samet Efe Ayverdi <efe.ayverdi@ug.bilkent.edu.tr>
2026-01-28 15:49:06 +0300

    Added User Prompting
    The code now prompts the user to pick an array size, then prompts the user to pick from a selection of operations, quits the program if the selected operation is 7.

--- collabWork.java
+++ collabWork.java
@@ -1,9 +1,69 @@
-import java.util.ArrayList;
+import java.util.Scanner;
 
-public class collabWork {
+public class CollabWork {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int arraySize;
+        int chosenOperation = 0; //temp value
+        boolean isOver = false;
 
-        
+        do { //Until user gives a non-negative integer
+            System.out.print("Give an array size (non-negative integer): ");
+            //Until user gives a proper integer
+            while (!in.hasNextInt()) {
+                System.out.print("Please enter a valid integer: ");
+                in.nextLine();
+            }
+            arraySize = in.nextInt();
+            in.nextLine();
+        } while (arraySize < 0);
+        //****** createArray(arraySize); 
+
+        //Prompt the user again and again until the user quits
+        do {
+            //Inform the user
+            System.out.println("\n---------Operations---------");
+            System.out.println("1. Find the array's minimum value");
+            System.out.println("2. Find the array's maximum value");
+            System.out.println("3. Find the average of the array");
+            System.out.println("4. See how far each element of the array is to the average");
+            System.out.println("5. Find the sum of elements in odd-numbered spaces");
+            System.out.println("6. Find the sum of elements in even-numbered spaces");
+            System.out.println("7. Quit\n");
+
+            do { //Until the user gives an integer in range 1-7
+                System.out.print("Pick an operation (1-7): ");
+                while (!in.hasNextInt()) { //Until the user gives an integer
+                    System.out.print("Please enter a valid operation number (1-7): ");
+                    in.nextLine();
+                }
+                chosenOperation = in.nextInt();
+                in.nextLine();
+            } while (chosenOperation < 1 || chosenOperation > 7);
+
+            if (chosenOperation == 1) {
+
+            }
+            else if (chosenOperation == 2) {
+
+            }
+            else if (chosenOperation == 3) {
+
+            }
+            else if (chosenOperation == 4) {
+
+            }
+            else if (chosenOperation == 5) {
+
+            }
+            else if (chosenOperation == 6) {
+
+            }
+            else if (chosenOperation == 7) {
+                System.out.println("Quitting...");
+                isOver = true;
+            }
+        } while (!isOver);
     }
 }
